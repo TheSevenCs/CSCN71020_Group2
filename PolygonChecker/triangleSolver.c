28544eae661f7c1b38db16744a2391003e1d678d
Yuval Rashish <yrashish3335@conestogac.on.ca>
2023-11-03 14:43:01 -0400

    Added function to triangle solver
    New function that checks if a triangle is a triangle based off the prove that the sum of two sides are greater than the side of the hypotenuse

--- PolygonChecker/triangleSolver.c
+++ PolygonChecker/triangleSolver.c
@@ -2,7 +2,7 @@
 #include <stdbool.h>
 
 #include "triangleSolver.h"
-
+#define NUM_OF_SIDES_ON_TRIANGLE 3
 char* analyzeTriangle(int side1, int side2, int side3) {
 	char* result = "";
 	if (side1 <= 0 || side2 <= 0 || side3 <= 0) {
@@ -21,4 +21,32 @@ char* analyzeTriangle(int side1, int side2, int side3) {
 	}
 
 	return result;
+}
+
+bool isATriangle(int* sides)
+{
+	int hypotenuse = 0;
+	for (int i = 0; i < NUM_OF_SIDES_ON_TRIANGLE; i++)
+	{
+		if (hypotenuse < sides[i])
+		{
+			hypotenuse = sides[i];
+		}
+	}
+	if (sides[0] + sides[1] + sides[2] - hypotenuse >= hypotenuse)
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
+int* angleCalculator(int* sides)
+{
+	if (!isATriangle)
+	{
+		return -1;
+	}
+
 }
