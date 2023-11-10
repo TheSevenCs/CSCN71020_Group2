eaa3f680bc09e4e023c3983eb66baa9322f5ac81
Yuval Rashish <yrashish3335@conestogac.on.ca>
2023-11-10 14:01:43 -0500

    Finished triangle angle calculater
    calculates all 3 angles of the triangle now
    in addition I also made some small changes to the quad solver comments and made the pi definition in the .h so I could use it in the triangle solver

--- PolygonChecker/quadrilateralSolver.c
+++ PolygonChecker/quadrilateralSolver.c
@@ -27,23 +27,23 @@ char* analyzeRectangle(POINT* arr) {
 	VECTOR ac = CreateVectorByPoint(*(arr), *(arr + 2));
 	VECTOR ad = CreateVectorByPoint(*(arr), *(arr + 3));
 
-	double angle_bac = getCornerByTwoVector(ab, ac); // angle of ］bac
+	double angle_bac = getCornerByTwoVector(ab, ac); // angle of bac
 
-	double angle_bad = getCornerByTwoVector(ab, ad); // angle of ］bad
+	double angle_bad = getCornerByTwoVector(ab, ad); // angle of bad
 
-	double angle_cad = getCornerByTwoVector(ac, ad); // angle of ］cad
+	double angle_cad = getCornerByTwoVector(ac, ad); // angle of cad
 
-	if (fabs(angle_bac - 90) < ACCURACY) { // ］bac = 90,  if vector ab = cd, then the shape is rectangle
+	if (fabs(angle_bac - 90) < ACCURACY) { // angle bac = 90,  if vector ab = cd, then the shape is rectangle
 		VECTOR cd = CreateVectorByPoint(*(arr + 2), *(arr + 3));
 		if (IsVectorEqual(ab, cd)) 
 			is_rectangle = true;
 	}
-	else if (fabs(angle_bad - 90) < ACCURACY) { // ］bad = 90,  if vector ab = dc, then the shape is rectangle
+	else if (fabs(angle_bad - 90) < ACCURACY) { // angle bad = 90,  if vector ab = dc, then the shape is rectangle
 		VECTOR dc = CreateVectorByPoint(*(arr + 3), *(arr + 2));
 		if (IsVectorEqual(ab, dc))
 			is_rectangle = true;
 	}
-	else if (fabs(angle_cad - 90) < ACCURACY) { // ］cad = 90,  if vector ac = db, then the shape is rectangle
+	else if (fabs(angle_cad - 90) < ACCURACY) { // angle cad = 90,  if vector ac = db, then the shape is rectangle
 		VECTOR db = CreateVectorByPoint(*(arr + 3), *(arr + 1));
 		if (IsVectorEqual(ac, db))
 			is_rectangle = true;
--- PolygonChecker/quadrilateralSolver.h
+++ PolygonChecker/quadrilateralSolver.h
@@ -1,7 +1,8 @@
 //CSCN71020 - Group2 - zongping cui - 05 NOV 
 #pragma once
 
-#include "point.h"
 
+#include "point.h"
+#define M_PI 3.14159265358979323846
 
 char* analyzeRectangle(POINT* a);
